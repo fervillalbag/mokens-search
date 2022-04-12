f0e4e1c49924c6aa6ae62ab2f53f632ea372e582
fervillalbag <21fervillalbag@gmail.com>
2022-04-12 16:09:10 -0400

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Starting the project</h1>
+      <p className="text-9xl text-red-700">Hello</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{tsx,ts}',
+    './src/components/**/*.{tsx,ts}',
+    './src/layout/**/*.{tsx,ts}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
